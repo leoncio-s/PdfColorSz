84d087a77dfa6e783881f90674fe78811e6e7b4f
Leoncio Souza <59627510+leoncio-s@users.noreply.github.com>
2021-03-12 12:21:41 -0300

    Delete PDFColorSz_1.2.0 directory

--- PDFColorSz_1.2.0/Example.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("./PDFColorSz.php");
-
-    $pdf = new PDFColorSz("./Teste.pdf"); // Retorna um arquivo json
-
-    $values = Json_decode($pdf->values); // Converte o json em array
-
-    print_r($values);
-?>
