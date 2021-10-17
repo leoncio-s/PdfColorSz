e4ce893a0e94597ade9dd6836832d8b5a105b30c
Leoncio Souza <59627510+leoncio-s@users.noreply.github.com>
2021-10-17 19:49:03 -0300

    Delete PDFColorSz_1.0.0/vendor directory

--- PDFColorSz_1.0.0/vendor/howtomakeaturn/pdfinfo/src/Howtomakeaturn/PDFInfo/Exceptions/OpenPDFException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Howtomakeaturn\PDFInfo\Exceptions;
-
-use \Exception;
-
-class OpenPDFException extends Exception{};
--- PDFColorSz_1.0.0/vendor/howtomakeaturn/pdfinfo/src/Howtomakeaturn/PDFInfo/Exceptions/OtherException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Howtomakeaturn\PDFInfo\Exceptions;
-
-use \Exception;
-
-class OtherException extends Exception{};
